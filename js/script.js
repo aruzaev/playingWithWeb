045f2e5ae269ccc858c84d8a2c4f6148730e9145
Artem Ruzaev <ruzaev.artem21@gmail.com>
2022-06-16 11:27:39 -0400

    type conversion

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
 "use strict";
 
 (function() {
-    let name = prompt("What is your name?");
-    let confirmUserName = confirm(`Ah, so you're ${name}?`);
-
-    if (confirmUserName) {
-        alert(`Nice to meet you ${name}!`);
-    }
-    else {
-        alert("Stop messing with me, you hooligan. Get the fuck out now!!");
-    }
+    let age = Number("42     ");
+    alert(age);
 })();
 
 
