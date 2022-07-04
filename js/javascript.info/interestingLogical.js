c242dc8ca5bfdfc083ab1bac8f77af295d594ad1
Artem Ruzaev <ruzaev.artem21@gmail.com>
2022-07-04 10:39:27 -0400

    Added info about ! operator

--- js/javascript.info/interestingLogical.js
+++ js/javascript.info/interestingLogical.js
@@ -10,4 +10,15 @@
 
     // the same thing could be done for the && operator, except && returns the first 
     // true value
+
+    console.log(!true); // false
+    console.log(!0); // true
+
+    console.log(!!"This will get converted into a boolean type");
+    // this is because the first ! will make it false, while the second !
+    // will make the boolean into what it really is (which is a true since string isnt
+    // empty)
+
+    console.log(!!"");
+
 })();
